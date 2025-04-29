0f8abc1311a7dd0161b6d068090638086ef373aa
tusharchaudhari30 <chaudharitushar2077@gmail.com>
2025-04-29 23:36:44 +0530

    kakfa fixed

--- stock-data-feed/src/test/java/com/theboringproject/stock_data_feed/StockDataFeedApplicationTests.java
+++ stock-data-feed/src/test/java/com/theboringproject/stock_data_feed/StockDataFeedApplicationTests.java
@@ -1,13 +1,12 @@
 package com.theboringproject.stock_data_feed;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@ImportAutoConfiguration(exclude = KafkaAutoConfiguration.class)
 class StockDataFeedApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
